fd6b5329cd5b4290df0b3db8dc3facc49639379b
NathanDescamps <95073151+NathanDescamps@users.noreply.github.com>
2022-11-10 08:52:05 +0100

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,25 @@
+import time
+import concurrent.futures
+import requests
+
+if __name__ == 'main2':
+
+    img_urls = [
+        'https: // cdn.pixabay.com / photo / 2022 / 10 / 31 / 18 / 44 / spider - web - 7560535_960_720.j
+    pg''
+    ]
+
+    def download_image(img_url):
+        img_bytes = requests.get(img_url).content
+        img_name = img_url.split('/')[9]
+        with open(img_name, 'wb') as img_file:
+            img_file.write(img_bytes)
+            print(f"{img_name} was downloaded")
+
+    start = time.perf_counter()
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(download_image, img_urls)
+
+    end = time.perf_counter()
+    print(f"Tasks ended in {round(end - start, 2)} second(s)")
